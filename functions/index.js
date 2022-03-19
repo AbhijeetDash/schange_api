04c5266d5ac0fe3557594797ff68f764375a37e4
Abhijeet Dash <abhijeetdash@Abhijeets-MacBook-Pro.local>
2022-03-19 19:42:33 +0530

    added basic functions

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,38 @@
+/* eslint-disable no-unused-vars */
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Initialization of firebase app
+const app = admin.initializeApp();
+
+// Getting firestore instance
+const firestore = admin.firestore();
+
+/**
+ * Validates the API Key.
+ * @param {string} clientApiKey The Api Key that the client has.
+ * @return {boolean} The sum of the two numbers.
+ */
+function isVerifiedApiUser(clientApiKey) {
+  if (clientApiKey == "lNsu201037cwQecWRac7") {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+exports.sendInvoiceToUser = functions.https.onRequest((request, response) => {
+  const data = request.body;
+  const clientApiKey = data.api;
+  const verified = isVerifiedApiUser(clientApiKey);
+  if (verified) {
+    return response.send({
+      "verification": verified,
+    });
+  } else {
+    return response.send({
+      "error": "verification_failed",
+      "description": "please make sure to renew your key.",
+    });
+  }
+});
